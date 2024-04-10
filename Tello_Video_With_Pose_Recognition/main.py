455db25a862f45f0a768ee89d5469224937bc685
Khushboo Jayan <khushboojayan@gmail.com>
2024-04-10 05:26:41 +0100

    intercept-conntrol-attacks

--- Tello_Video_With_Pose_Recognition/main.py
+++ Tello_Video_With_Pose_Recognition/main.py
@@ -3,12 +3,12 @@ from tello_control_ui import TelloUI
 
 
 def main():
+    drone = tello.Tello('', 8889)
+    vplayer = TelloUI(drone, "./img/")
 
-    drone = tello.Tello('', 8889)  
-    vplayer = TelloUI(drone,"./img/")
-    
-	# start the Tkinter mainloop
-    vplayer.root.mainloop() 
+    # start the Tkinter mainloop
+    vplayer.root.mainloop()
+
 
 if __name__ == "__main__":
     main()
